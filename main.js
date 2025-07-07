503ac43fc4cfbca534c012593ef9f0fadb1d1ae1
santiagodonadio <sdonadio@worcester.edu>
2025-07-07 16:30:50 -0400

    Comments for initial setup

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const eightBall = document.getElementById("eightBall");
 const responseBall = document.getElementById("eightBallResponse");
 
-// On click, shake then switch
+
 eightBall.addEventListener("click", () => {
   eightBall.classList.add("shake");
 
-  // Wait for animation duration (adjust to match your CSS)
+   
   setTimeout(() => {
     eightBall.style.display = "none";
     responseBall.style.display = "block";
